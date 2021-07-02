f53bbb84b12c7fb2492d0a7bf97aa695875f3b99
CubersHub <71025646+CubersHub@users.noreply.github.com>
2021-07-02 18:36:37 +0200

    Update cmdrc.cmd

--- cmdrc.cmd
+++ cmdrc.cmd
@@ -1,1 +1,1 @@
->NUL CHCP 65001&PROMPT $E[0m$E[34m┌──$E[0m$E[36m[$T$H$H$H]$E[0m$E[34m─($E[0m$E[34;1m%USERNAME%@%USERDOMAIN%$E[0m$E[34m)─[$E[0m$E[32m$P$E[0m$E[34m]$_└───╼$E[0m$E[38;5;208m$$$S$E[0m
+>NUL CHCP 65001&PROMPT $E[0m$E[34m┌──$E[0m$E[36m[$T$H$H$H]$E[0m$E[34m─($E[0m$E[34;1m!USERNAME!@!USERDOMAIN!$E[0m$E[34m)─[$E[0m$E[32m$S$P$S$E[0m$E[34m]!Line!$_└───╼$E[0m$E[38;5;208m$$$S$E[0m
